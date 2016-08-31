82039cfb4879e09a011cdbeabcc027e4feb2e868
Qsaka <949241796@qq.com>
2016-08-31 20:17:42 +0800

    update

---
+++ algorithms/268. Missing Number.py
@@ -0,0 +1,23 @@
+# Given an array containing n distinct numbers taken from 0, 1, 2, ..., n,
+# find the one that is missing from the array.
+
+# For example,
+# Given nums = [0, 1, 3] return 2.
+
+# Note:
+# Your algorithm should run in linear runtime complexity. Could you
+# implement it using only constant extra space complexity?
+
+
+class Solution(object):
+
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        for i in range(len(nums)):
+            if i != nums[i]:
+                return i
+        return i + 1
