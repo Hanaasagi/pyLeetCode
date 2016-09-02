75815d02926baa29f9799eaebf5384b2f5f361ce
Qsaka <949241796@qq.com>
2016-09-02 10:53:17 +0800

    update

---
+++ algorithms/290. Word Pattern.py
@@ -0,0 +1,39 @@
+# Given a pattern and a string str, find if str follows the same pattern.
+
+# Here follow means a full match, such that there is a bijection between a
+# letter in pattern and a non-empty word in str.
+
+# Examples:
+
+#     pattern = "abba", str = "dog cat cat dog" should return true.
+#     pattern = "abba", str = "dog cat cat fish" should return false.
+#     pattern = "aaaa", str = "dog cat cat dog" should return false.
+#     pattern = "abba", str = "dog dog dog dog" should return false.
+
+# Notes:
+# You may assume pattern contains only lowercase letters, and str contains
+# lowercase letters separated by a single space.
+
+
+class Solution(object):
+
+    def wordPattern(self, pattern, str):
+        """
+        :type pattern: str
+        :type str: str
+        :rtype: bool
+        """
+        dic = {}
+        parr = list(pattern)
+        sarr = str.split()
+        if len(parr) != len(sarr):
+            return False
+        for i in range(len(parr)):
+            if parr[i] not in dic:
+                if sarr[i] in dic.values():
+                    return False
+                dic[parr[i]] = sarr[i]
+            elif dic[parr[i]] != sarr[i]:
+                return False
+
+        return True
